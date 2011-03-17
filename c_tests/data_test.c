0681ad5622551e02aa4589a562cad1694209aa1b
Daniel Waterworth <da.waterworth@googlemail.com>
2011-03-17 07:59:42 +0000

    added a aodbm_data test

--- c_tests/data_test.c
+++ c_tests/data_test.c
@@ -20,7 +20,31 @@
 #include "aodbm_data.h"
 
 START_TEST (test_1) {
+    aodbm_data *a = aodbm_construct_data("hello", 5);
+    fail_unless(a->sz == 5);
+    fail_unless(memcmp("hello", a->dat, 5) == 0);
     
+    aodbm_data *b = aodbm_data_dup(a);
+    fail_unless(b->sz == 5);
+    fail_unless(memcmp("hello", b->dat, 5) == 0);
+    
+    fail_unless(aodbm_data_eq(a, b));
+    fail_unless(aodbm_data_cmp(a, b) == 0);
+    aodbm_free_data(a);
+    
+    a = aodbm_data_from_str("hello");
+    fail_unless(a->sz == 5);
+    fail_unless(memcmp("hello", a->dat, 5) == 0);
+    
+    aodbm_free_data(a);
+    aodbm_free_data(b);
+    
+    a = aodbm_data_empty();
+    b = aodbm_data_empty();
+    fail_unless(aodbm_data_eq(a, b));
+    
+    aodbm_free_data(a);
+    aodbm_free_data(b);
 } END_TEST
 
 TCase *data_test_case() {
